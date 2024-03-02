23036c7c0f9d8cca76e1f53386661a5bfd85afa2
congle-engineer <congle-engineer@gmail.com>
2024-03-02 21:06:19 +0700

    Update

---
+++ myapp/app.middleware.2.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/user/:id', (req, res, next) => {
+  if (req.params.id === '0') {
+    next('route')
+  } else {
+    next()
+  }
+}, (req, res, next) => {
+  res.send('regular')
+})
+
+app.get('/user/:id', (req, res, next) => {
+  res.send('special')
+})
+
+app.listen(3000, () => {
+  console.log(`Start listening on port 3000`)
+})
